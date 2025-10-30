9d1baeb71dfbda221dd9af1cbb9c162afa58df63
CT02k <joaopaulocfwork@gmail.com>
2025-10-30 19:21:03 -0300

    refactor(quiz): move quiz creation form logic to a hook

--- app/quiz/create/page.tsx
+++ app/quiz/create/page.tsx
@@ -1,56 +1,16 @@
 "use client";
-import { useState } from "react";
-
-type OptionData = { text: string; isCorrect: boolean };
-type QuestionData = { text: string; options: OptionData[] };
-type QuizData = { title: string; questions: QuestionData[] };
+import { useQuiz } from "./hooks/useQuiz";
 
 export default function CreateQuizPage() {
-  const [quiz, setQuiz] = useState<QuizData>({
-    title: "",
-    questions: [],
-  });
-
-  const addQuestion = () => {
-    const newQuestions = [
-      ...quiz.questions,
-      {
-        text: "",
-        options: [
-          { text: "", isCorrect: false },
-          { text: "", isCorrect: false },
-        ],
-      },
-    ];
-    setQuiz({ ...quiz, questions: newQuestions });
-  };
-
-  const updateQuestionText = (qi: number, text: string) => {
-    const newQuestions = [...quiz.questions];
-    newQuestions[qi].text = text;
-    setQuiz({ ...quiz, questions: newQuestions });
-  };
-
-  const addOption = (qi: number) => {
-    const newQuestions = [...quiz.questions];
-    newQuestions[qi].options.push({ text: "", isCorrect: false });
-    setQuiz({ ...quiz, questions: newQuestions });
-  };
-
-  const updateOptionText = (qi: number, oi: number, text: string) => {
-    const newQuestions = [...quiz.questions];
-    newQuestions[qi].options[oi].text = text;
-    setQuiz({ ...quiz, questions: newQuestions });
-  };
-
-  const markCorrect = (qi: number, oi: number) => {
-    const newQuestions = [...quiz.questions];
-    newQuestions[qi].options = newQuestions[qi].options.map((opt, i) => ({
-      ...opt,
-      isCorrect: i === oi,
-    }));
-    setQuiz({ ...quiz, questions: newQuestions });
-  };
+  const {
+    quiz,
+    setQuiz,
+    updateQuestionText,
+    markCorrect,
+    updateOptionText,
+    addOption,
+    addQuestion,
+  } = useQuiz();
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
